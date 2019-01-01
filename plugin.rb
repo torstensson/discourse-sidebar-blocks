5d3787616e4a550b2b99a2b76ab9aee9e8b355ad
Per Torstensson <per.torstensson@gmail.com>
2019-01-01 21:11:45 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-sidebar-blocks
-# about: add a sidebar to topic lists (discovery) with several blocks available: posts from a category, recent replies
-# version: 0.1
-# authors: pmusaraj
-# url: https://github.com/pmusaraj/discourse-home-sidebar
+# about: add two column sidebar to topic lists (discovery) with several blocks available: posts from a category, recent replies
+# version: 0.2
+# authors: pmusaraj, torstensson
+# url: https://github.com/torstensson/discourse-sidebar-blocks
 
 register_asset "stylesheets/sidebar.scss"
